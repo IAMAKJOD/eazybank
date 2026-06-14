468b04e96e2578c0589478a3dfd2a37603c4b6ac
IAMAKJOD <karadeaditya2015@gmail.com>
2026-06-14 15:37:51 +0530

    Added Client side service discovery and registratin with Eureka

--- accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -10,11 +10,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = AccountsContactInfoDto.class)
+@EnableFeignClients
 @OpenAPIDefinition(
         info = @Info(
                 title = "Accounts microservice REST API Documentation",
